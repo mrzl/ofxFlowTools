dc63fa619e4b41ff2be1fbc4b240b73a98183552
Marcel Schwittlick <lzrm@mrzl.net>
2016-02-13 15:09:34 +0100

    mostly reformatting, adding some project specific stuff, too

--- src/drawforces/ftDrawMouseForces.cpp
+++ src/drawforces/ftDrawMouseForces.cpp
@@ -10,136 +10,174 @@
 
 
 namespace flowTools {
-	
+
 	ftDrawMouseForces::ftDrawMouseForces() {
-		ofAddListener(ofEvents().mouseMoved, this, &ftDrawMouseForces::mouseMoved);
-		ofAddListener(ofEvents().mouseDragged, this, &ftDrawMouseForces::mouseDragged);
+		ofAddListener( ofEvents().mouseMoved, this, &ftDrawMouseForces::mouseMoved );
+		ofAddListener( ofEvents().mouseDragged, this, &ftDrawMouseForces::mouseDragged );
 	}
-    
-    ftDrawMouseForces::~ftDrawMouseForces() {
-        ofRemoveListener(ofEvents().mouseMoved, this, &ftDrawMouseForces::mouseMoved);
-        ofRemoveListener(ofEvents().mouseDragged, this, &ftDrawMouseForces::mouseDragged);
-    }
-	
-	void ftDrawMouseForces::setup(int _simulationWidth, int _simulationHeight, int _densityWidth, int _densityHeight) {
+
+	ftDrawMouseForces::~ftDrawMouseForces() {
+		ofRemoveListener( ofEvents().mouseMoved, this, &ftDrawMouseForces::mouseMoved );
+		ofRemoveListener( ofEvents().mouseDragged, this, &ftDrawMouseForces::mouseDragged );
+	}
+
+	void ftDrawMouseForces::setup( int _simulationWidth, int _simulationHeight, int _densityWidth, int _densityHeight ) {
 		simulationWidth = _simulationWidth;
 		simulationHeight = _simulationHeight;
-		densityWidth = (!_densityWidth)? simulationWidth : _densityWidth;
-		densityHeight = (!_densityHeight)? simulationHeight: _densityHeight;
-		
-		numDrawForces = 6;
-		drawForces = new ftDrawForce[numDrawForces];
-		drawForces[0].setup(densityWidth, densityHeight, FT_DENSITY, true);
-		drawForces[0].setName("draw full res");
-		drawForces[1].setup(simulationWidth, simulationHeight, FT_VELOCITY, true);
-		drawForces[1].setName("draw flow res 1");
-		drawForces[2].setup(simulationWidth, simulationHeight, FT_TEMPERATURE, true);
-		drawForces[2].setName("draw flow res 2");
-		drawForces[3].setup(densityWidth, densityHeight, FT_DENSITY, false);
-		drawForces[3].setName("draw full res");
-		drawForces[4].setup(simulationWidth, simulationHeight, FT_VELOCITY, false);
-		drawForces[4].setName("draw flow res 1");
-		drawForces[5].setup(simulationWidth, simulationHeight, FT_TEMPERATURE, false);
-		drawForces[5].setName("draw flow res 2");
-		
-		leftButtonParameters.setName("mouse left button");
-		leftButtonParameters.add(doResetDrawForces.set("reset", false));
-		rightButtonParameters.setName("mouse right button");
-		rightButtonParameters.add(doResetDrawForces.set("reset", false));
-		doResetDrawForces.addListener(this, &ftDrawMouseForces::resetDrawForcesListner);
-		for (int i=0; i<3; i++) {
-			leftButtonParameters.add(drawForces[i].parameters);
-			rightButtonParameters.add(drawForces[i+3].parameters);
+		densityWidth = ( !_densityWidth ) ? simulationWidth : _densityWidth;
+		densityHeight = ( !_densityHeight ) ? simulationHeight : _densityHeight;
+
+		numDrawForces = 8;
+		drawForces = new ftDrawForce[ numDrawForces ];
+		drawForces[ 0 ].setup( densityWidth, densityHeight, FT_DENSITY, true );
+		drawForces[ 0 ].setName( "draw full res" );
+		drawForces[ 1 ].setup( simulationWidth, simulationHeight, FT_VELOCITY, true );
+		drawForces[ 1 ].setName( "draw flow res 1" );
+		drawForces[ 2 ].setup( simulationWidth, simulationHeight, FT_TEMPERATURE, true );
+		drawForces[ 2 ].setName( "draw flow res 2" );
+		drawForces[ 3 ].setup( simulationWidth, simulationHeight, FT_OBSTACLE, true );
+		drawForces[ 3 ].setName( "draw flow res 3" );
+		drawForces[ 4 ].setup( densityWidth, densityHeight, FT_DENSITY, false );
+		drawForces[ 4 ].setName( "draw full res" );
+		drawForces[ 5 ].setup( simulationWidth, simulationHeight, FT_VELOCITY, false );
+		drawForces[ 5 ].setName( "draw flow res 1" );
+		drawForces[ 6 ].setup( simulationWidth, simulationHeight, FT_TEMPERATURE, false );
+		drawForces[ 6 ].setName( "draw flow res 2" );
+		drawForces[ 7 ].setup( simulationWidth, simulationHeight, FT_OBSTACLE, false );
+		drawForces[ 7 ].setName( "draw flow res 3" );
+
+		leftButtonParameters.setName( "mouse left button" );
+		leftButtonParameters.add( doResetDrawForces.set( "reset", false ) );
+		rightButtonParameters.setName( "mouse right button" );
+		rightButtonParameters.add( doResetDrawForces.set( "reset", false ) );
+		doResetDrawForces.addListener( this, &ftDrawMouseForces::resetDrawForcesListner );
+		for( int i = 0; i < numDrawForces / 2; i++ ) {
+			leftButtonParameters.add( drawForces[ i ].parameters );
+			rightButtonParameters.add( drawForces[ i + ( numDrawForces / 2 ) ].parameters );
 		}
 	}
-	
-	void ftDrawMouseForces::update(float _deltaTime) {
+
+	void ftDrawMouseForces::update( float _deltaTime ) {
 		deltaTime = _deltaTime;
-		
-		for (int i=0; i<numDrawForces; i++) {
-			drawForces[i].update();
+
+		for( int i = 0; i < numDrawForces; i++ ) {
+			drawForces[ i ].update();
 		}
 	}
-	
+
 	//--------------------------------------------------------------
-	bool ftDrawMouseForces::didChange(int _index) {
-		if (_index < 0 || _index >= numDrawForces) {
-			ofLogWarning("ftDrawMouseForces::getDrawForceType: index out of range");
+	bool ftDrawMouseForces::didChange( int _index ) {
+		if( _index < 0 || _index >= numDrawForces ) {
+			ofLogWarning( "ftDrawMouseForces::getDrawForceType: index out of range" );
 			return false;
 		}
 		else
-			return drawForces[_index].didChange();
+			return drawForces[ _index ].didChange();
 	}
-	
+
 	//--------------------------------------------------------------
-	ftDrawForceType ftDrawMouseForces::getType(int _index) {
-		if (_index < 0 || _index >= numDrawForces) {
-			ofLogWarning("ftDrawMouseForces::getDrawForceType: index out of range");
+	ftDrawForceType ftDrawMouseForces::getType( int _index ) {
+		if( _index < 0 || _index >= numDrawForces ) {
+			ofLogWarning( "ftDrawMouseForces::getDrawForceType: index out of range" );
 			return FT_NONE;
 		}
 		else
-			return drawForces[_index].getType();
+			return drawForces[ _index ].getType();
 	}
-	
+
+	void ftDrawMouseForces::setType( int _index, ftDrawForceType _type )
+	{
+		if( _index < 0 || _index >= numDrawForces ) {
+			ofLogWarning( "ftDrawMouseForces::setType: index out of range" );
+		}
+		else {
+			drawForces[ _index ].setType( _type );
+		}
+	}
+
 	//--------------------------------------------------------------
-	ofTexture& ftDrawMouseForces::getTextureReference(int _index) {
-		if (_index < 0 || _index >= numDrawForces) {
-			ofLogError("ftDrawMouseForces::getTexture: index out of range");
+	ofTexture& ftDrawMouseForces::getTextureReference( int _index ) {
+		if( _index < 0 || _index >= numDrawForces ) {
+			ofLogError( "ftDrawMouseForces::getTexture: index out of range" );
 		}
 		else
-			return drawForces[_index].getTexture();
-		
+			return drawForces[ _index ].getTexture();
+
 	}
-	
+
 	//--------------------------------------------------------------
-	float ftDrawMouseForces::getStrength(int _index) {
-		if (_index < 0 || _index >= numDrawForces) {
-			ofLogWarning("ftDrawMouseForces::getStrength: index out of range");
+	float ftDrawMouseForces::getStrength( int _index ) {
+		if( _index < 0 || _index >= numDrawForces ) {
+			ofLogWarning( "ftDrawMouseForces::getStrength: index out of range" );
 			return 0;
 		}
 		else {
-			if (drawForces[_index].getIsTemporary()) {
-				return drawForces[_index].getStrength();
+			if( drawForces[ _index ].getIsTemporary() ) {
+				return drawForces[ _index ].getStrength();
 			}
 			else {
-				return drawForces[_index].getStrength() * deltaTime;;
+				return drawForces[ _index ].getStrength() * deltaTime;;
 			}
 		}
 	}
-	
+
 	//--------------------------------------------------------------
 	void ftDrawMouseForces::mouseDragged( ofMouseEventArgs& mouse ) {
 		ofVec2f normalizedMouse;
-		
-		normalizedMouse.set(mouse.x / (float)ofGetWindowWidth(), mouse.y / (float)ofGetWindowHeight());
-		
+
+		normalizedMouse.set( mouse.x / ( float ) ofGetWindowWidth(), mouse.y / ( float ) ofGetWindowHeight() );
+
 		ofVec2f velocity = normalizedMouse - lastNormalizedMouse;
-		
-		if (mouse.button == 0) {
-			
-			for (int i=0; i<3; i++) {
-				if (drawForces[i].getType() == FT_VELOCITY)
-					drawForces[i].setForce(velocity);
-				drawForces[i].applyForce(normalizedMouse);
+
+		if( mouse.button == 0 ) {
+
+			// -1 in order to skip the automatic obstacle drawings
+			for( int i = 0; i < numDrawForces / 2 - 1; i++ ) {
+				if( drawForces[ i ].getType() == FT_VELOCITY )
+					drawForces[ i ].setForce( velocity );
+				drawForces[ i ].applyForce( normalizedMouse );
 			}
 		}
 		else {
-			
-			for (int i=3; i<numDrawForces; i++) {
-				if (drawForces[i].getType() == FT_VELOCITY)
-					drawForces[i].setForce(velocity);
-				drawForces[i].applyForce(normalizedMouse);
+			// -1 in order to skip the automatic obstacle drawings
+			for( int i = numDrawForces / 2; i < numDrawForces - 1; i++ ) {
+				if( drawForces[ i ].getType() == FT_VELOCITY )
+					drawForces[ i ].setForce( velocity );
+				drawForces[ i ].applyForce( normalizedMouse );
 			}
 		}
-		lastNormalizedMouse.set(normalizedMouse);
-		
+		lastNormalizedMouse.set( normalizedMouse );
+
 	}
-	
-	//--------------------------------------------------------------
-	void ftDrawMouseForces::mouseMoved( ofMouseEventArgs& mouse ){
+
+	void ftDrawMouseForces::permanentImpulse( int x, int y, float direction, bool isObstacle )
+	{
 		ofVec2f normalizedMouse;
-		normalizedMouse.set(mouse.x / (float)ofGetWindowWidth(), mouse.y / (float)ofGetWindowHeight());
-		lastNormalizedMouse.set(normalizedMouse);
+
+		normalizedMouse.set( x / ( float ) ofGetWindowWidth(), y / ( float ) ofGetWindowHeight() );
+
+		// fucking test this shit when you get back
+		ofVec2f last( normalizedMouse.x + 0.1f * cos( direction ), normalizedMouse.y + 0.1f * sin( direction ) );
+		ofVec2f velocity = normalizedMouse - last;
+
+		if( isObstacle ) {
+			drawForces[ 7 ].applyForce( normalizedMouse );
+		}
+		else {
+			for( int i = numDrawForces / 2; i < numDrawForces - 1; i++ ) {
+				if( drawForces[ i ].getType() == FT_VELOCITY ) {
+					drawForces[ i ].setForce( velocity );
+				}
+
+				drawForces[ i ].applyForce( normalizedMouse );
+			}
+		}
 	}
-	
+
+	//--------------------------------------------------------------
+	void ftDrawMouseForces::mouseMoved( ofMouseEventArgs& mouse ) {
+		ofVec2f normalizedMouse;
+		normalizedMouse.set( mouse.x / ( float ) ofGetWindowWidth(), mouse.y / ( float ) ofGetWindowHeight() );
+		lastNormalizedMouse.set( normalizedMouse );
+	}
 }
--- src/drawforces/ftDrawMouseForces.h
+++ src/drawforces/ftDrawMouseForces.h
@@ -4,48 +4,49 @@
 #include "ftFbo.h"
 #include "ftDrawForce.h"
 
-
 namespace flowTools {
-	
+
 	class ftDrawMouseForces {
-		
+
 	public:
 		ftDrawMouseForces();
-        ~ftDrawMouseForces();
-	
-		void				setup(int _simulationWidth, int _simulationHeight, int _densityWidth = 0, int _densityHeight = 0) ;
-		void				update(float _deltaTime);
-		void				reset()  { for (int i=0; i<numDrawForces; i++) drawForces[i].reset(); }
-		
+		~ftDrawMouseForces();
+
+		void				setup( int _simulationWidth, int _simulationHeight, int _densityWidth = 0, int _densityHeight = 0 );
+		void				update( float _deltaTime );
+		void				reset() { for( int i = 0; i < numDrawForces; i++ ) drawForces[ i ].reset(); }
+
 		int					getNumForces() { return numDrawForces; }
-		
-		bool				didChange(int _index);
-		ftDrawForceType		getType(int _index);
-		ofTexture&			getTextureReference(int _index);
-		float				getStrength(int _index);
-		
-//		ofParameterGroup	parameters;
+
+		bool				didChange( int _index );
+		ftDrawForceType		getType( int _index );
+		void				setType( int _index, ftDrawForceType _type );
+		ofTexture&			getTextureReference( int _index );
+		float				getStrength( int _index );
+
+		//		ofParameterGroup	parameters;
 		ofParameterGroup	leftButtonParameters;
 		ofParameterGroup	rightButtonParameters;
-		
+
+		void				mouseMoved( ofMouseEventArgs & mouse );
+		void				mouseDragged( ofMouseEventArgs & mouse );
+		void				permanentImpulse( int x, int y, float direction, bool isObstacle );
+
 	private:
 		ofParameter<bool>	doResetDrawForces;
-		void				resetDrawForcesListner(bool& _value) { if (_value) { reset(); }; doResetDrawForces.set(false); }
-		
+		void				resetDrawForcesListner( bool& _value ) { if( _value ) { reset(); }; doResetDrawForces.set( false ); }
+
 		int					numDrawForces;
 		ftDrawForce*		drawForces;
-		
+
 		float				deltaTime;
-		
+
 		int					simulationWidth;
 		int					simulationHeight;
 		int					densityWidth;
 		int					densityHeight;
-		
-		void				mouseMoved(ofMouseEventArgs & mouse);
-		void				mouseDragged(ofMouseEventArgs & mouse);
-		
+
 		ofVec2f				lastNormalizedMouse;
-		
+
 	};
 }
